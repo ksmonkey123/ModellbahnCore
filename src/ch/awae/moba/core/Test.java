a1b1d7398af8d35df192c960dad1fe6270c67edf
Andreas Wälchli <andreas.waelchli@me.com>
2017-04-07 09:45:17 +0200

    Moved Null Annotations to lib folder

--- src/ch/awae/moba/core/Test.java
+++ src/ch/awae/moba/core/Test.java
@@ -2,7 +2,6 @@ package ch.awae.moba.core;
 
 import java.io.IOException;
 
-import ch.awae.moba.core.spi.Host;
 import ch.awae.moba.core.spi.HostFactory;
 import ch.awae.moba.core.spi.SPIChannel;
 import ch.awae.moba.core.spi.SPIGroup;
@@ -11,13 +10,10 @@ public class Test {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		SPIGroup group = new SPIGroup();
 
-		Host a = HostFactory.createHost(SPIChannel.CH_0, group);
-		Host b = HostFactory.createHost(SPIChannel.CH_1, group);
-		Host c = HostFactory.createHost(SPIChannel.CH_2, group);
-		Host d = HostFactory.createHost(SPIChannel.CH_3, group);
-
-		b.write((short) 0xf00f, (byte) 0x00);
-		c.write((short) 0xf00f, (byte) 0xff);
+		HostFactory.createHost(SPIChannel.CH_0, group);
+		HostFactory.createHost(SPIChannel.CH_1, group);
+		HostFactory.createHost(SPIChannel.CH_2, group);
+		HostFactory.createHost(SPIChannel.CH_3, group);
 
 		SPIThread spi_thread = new SPIThread(group);
 
